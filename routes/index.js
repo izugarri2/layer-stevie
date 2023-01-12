fe09b251a3a866add065f10e32f0d2f193171e10
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-12 15:21:17 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 8100;
+var express = require('express');
+var router = express.Router();
 
-app.use(express.static(__dirname+'/public));
-app.set('x-powered-by', false);
-app.listen(port, () => console.log(`app listening on port ${port}!`));
+app.use(express.static(__dirname+'/public'));
+app.listen(8080);
+module.exports = router;
