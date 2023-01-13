9ffd9078a7664feb7f02141dc66ef41690b93873
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-13 13:01:38 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -29,10 +29,6 @@ module.exports = new Router()
     appShell('public/en/index.html')
   })
 
-.match('en/:path*en/:file([^\\.]+|)', ({ appShell, cache }) => {
-    cache(edgeOnly)
-    appShell('public/en/index.html')
-  })
 
  // match other assets such as favicon, manifest.json, etc
   .match('/:path*', ({ serveStatic, cache }) => {
